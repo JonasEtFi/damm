169429b67df28ab784106fc48124dbf673cc87e3
Jonas Fischer <jonas.fisch1809@web.de>
2024-02-15 13:15:02 +0100

    fixed error, that alpha is to small

---
+++ damm/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jonasfischer/damm/damm/src/dpmm.cpp" "CMakeFiles/main.dir/dpmm.cpp.o" "gcc" "CMakeFiles/main.dir/dpmm.cpp.o.d"
+  "/Users/jonasfischer/damm/damm/src/dpmmDir.cpp" "CMakeFiles/main.dir/dpmmDir.cpp.o" "gcc" "CMakeFiles/main.dir/dpmmDir.cpp.o.d"
+  "/Users/jonasfischer/damm/damm/src/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/Users/jonasfischer/damm/damm/src/niw.cpp" "CMakeFiles/main.dir/niw.cpp.o" "gcc" "CMakeFiles/main.dir/niw.cpp.o.d"
+  "/Users/jonasfischer/damm/damm/src/niwDir.cpp" "CMakeFiles/main.dir/niwDir.cpp.o" "gcc" "CMakeFiles/main.dir/niwDir.cpp.o.d"
+  "/Users/jonasfischer/damm/damm/src/normal.cpp" "CMakeFiles/main.dir/normal.cpp.o" "gcc" "CMakeFiles/main.dir/normal.cpp.o.d"
+  "/Users/jonasfischer/damm/damm/src/normalDir.cpp" "CMakeFiles/main.dir/normalDir.cpp.o" "gcc" "CMakeFiles/main.dir/normalDir.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
